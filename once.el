cbdecb24f183f92228b83bdb120723cb4d0db460
Martin Edström <meedstrom@runbox.eu>
2026-02-03 00:04:54 +0100

    Bump copyright year

--- once.el
+++ once.el
@@ -1,6 +1,6 @@
 ;;; once.el --- Add-hook and eval-after-load, but only once  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025 Martin Edström
+;; Copyright (C) 2025-2026 Martin Edström
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
